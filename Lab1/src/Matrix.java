4a35a850b8d0a68f6199d721fd9a0e769ba5ee65
MaxK789 <maksym.kapitulin@nure.ua>
2024-06-08 17:59:50 +0300

    test

--- Lab1/src/Matrix.java
+++ Lab1/src/Matrix.java
@@ -1,2 +1,44 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Matrix {
+    private int[][] matrix;
+    private int size;
+    private Random random;
+
+    public Matrix(int size) {
+        this.size = size;
+        this.matrix = new int[size][size];
+        this.random = new Random();
+    }
+
+    public void fillRandom() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = random.nextInt(2 * size + 1) - size;
+            }
+        }
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] newMatrix) {
+        this.matrix = newMatrix;
+    }
+
+    public void printMatrix() {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public void rearrangeByRowSum() {
+        Arrays.sort(matrix, (row1, row2) -> {
+            int sum1 = Arrays.stream(row1).sum();
+            int sum2 = Arrays.stream(row2).sum();
+            return Integer.compare(sum1, sum2);
+        });
+    }
 }
--- Lab1/src/MatrixTest.java
+++ Lab1/src/MatrixTest.java
@@ -1,2 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class MatrixTest {
+
+    @Test
+    public void testRearrangeByRowSum() {
+        int[][] initialMatrix = {
+                {2, -2, 3},//3
+                {1, 2, -3},//0
+                {-2, -3, 1}//-4
+        };
+
+        int[][] expectedMatrix = {
+                {-2, -3, 1},//-4
+                {1, 2, -3},//0
+                {2, -2, 3}//3
+        };
+
+        Matrix matrix = new Matrix(3);
+        matrix.setMatrix(initialMatrix);
+        matrix.rearrangeByRowSum();
+
+        assertArrayEquals(expectedMatrix, matrix.getMatrix());
+    }
 }
