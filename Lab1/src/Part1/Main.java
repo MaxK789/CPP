38c703221bc3f34f4e003bbdfc0a034cff8a4c88
MaxK789 <maksym.kapitulin@nure.ua>
2024-06-12 21:08:18 +0300

    Part 1 improvement

--- Lab1/src/Part1/Main.java
+++ Lab1/src/Part1/Main.java
@@ -6,8 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введіть розмір матриці n: ");
-        int n = scanner.nextInt();
+        int n = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print("Введіть розмір матриці n: ");
+            if (scanner.hasNextInt()) {
+                n = scanner.nextInt();
+                if (n > 1) {
+                    validInput = true;
+                } else {
+                    System.out.println("Введіть число більше 1.");
+                }
+            } else {
+                System.out.println("Некоректне введення.");
+                scanner.next();
+            }
+        }
 
         Matrix matrix = new Matrix(n);
         matrix.fillRandom();
