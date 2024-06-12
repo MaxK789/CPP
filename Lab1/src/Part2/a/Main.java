8df32b23bffab0d1435278ecd5d4004941048ecf
MaxK789 <maksym.kapitulin@nure.ua>
2024-06-12 21:23:34 +0300

    Part 2a improvement

--- Lab1/src/Part2/a/Main.java
+++ Lab1/src/Part2/a/Main.java
@@ -6,16 +6,55 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введіть кількість дробів: ");
-        int k = scanner.nextInt();
+        int k = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print("Введіть кількість дробів: ");
+            if (scanner.hasNextInt()) {
+                k = scanner.nextInt();
+                if (k % 2 == 0 && k > 0) {
+                    validInput = true;
+                } else {
+                    System.out.println("Введіть парне число більше 0.");
+                }
+            } else {
+                System.out.println("Некоректне введення.");
+                scanner.next();
+            }
+        }
 
         RationalFraction[] fractions = new RationalFraction[k];
 
         for (int i = 0; i < k; i++) {
-            System.out.print("Введіть чисельник дробу " + (i + 1) + ": ");
-            int m = scanner.nextInt();
-            System.out.print("Введіть знаменник дробу " + (i + 1) + ": ");
-            int n = scanner.nextInt();
+            int m = 0;
+            int n = 0;
+            boolean validFraction = false;
+
+            while (!validFraction) {
+                System.out.print("Введіть чисельник дробу " + (i + 1) + ": ");
+                if (scanner.hasNextInt()) {
+                    m = scanner.nextInt();
+                } else {
+                    System.out.println("Некоректне введення.");
+                    scanner.next();
+                    continue;
+                }
+
+                System.out.print("Введіть знаменник дробу " + (i + 1) + ": ");
+                if (scanner.hasNextInt()) {
+                    n = scanner.nextInt();
+                    if (n != 0) {
+                        validFraction = true;
+                    } else {
+                        System.out.println("Знаменник не може бути 0.");
+                    }
+                } else {
+                    System.out.println("Некоректне введення.");
+                    scanner.next();
+                }
+            }
+
             fractions[i] = new RationalFraction(m, n);
         }
 
